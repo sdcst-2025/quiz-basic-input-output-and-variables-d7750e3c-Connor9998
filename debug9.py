68f04d5b505b5ea7b7c8ddf50c1aaad0a154843f
Connor9998 <connorm298@deltalearns.ca>
2024-11-04 11:28:53 -0800

    done

--- debug9.py
+++ debug9.py
@@ -3,6 +3,8 @@
 Debug this program so that it runs
 """
 x = input("Enter a number")
+#convert to number
+x= float(x)
 y = 2
 answer = x+y
-print(The answer is + answer)
+print("The answer is",answer)
